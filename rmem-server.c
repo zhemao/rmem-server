36d6af588c17230efcb21fb825095f738f50d5fa
Howard Mao <zhehao.mao@gmail.com>
2015-04-09 14:06:17 -0700

    basic server implementation

--- rmem-server.c
+++ rmem-server.c
@@ -3,160 +3,193 @@
 
 #include "common.h"
 #include "messages.h"
+#include "rmem_table.h"
 
-#define MAX_FILE_NAME 256
+struct rmem_table rmem;
+pthread_mutex_t alloc_mutex;
 
 struct conn_context
 {
-  char *buffer;
-  struct ibv_mr *buffer_mr;
+	struct ibv_mr *rmem_mr;
 
-  struct message *msg;
-  struct ibv_mr *msg_mr;
+	struct message *recv_msg;
+	struct ibv_mr *recv_msg_mr;
 
-  int fd;
-  char file_name[MAX_FILE_NAME];
+	struct message *send_msg;
+	struct ibv_mr *send_msg_mr;
 };
 
 static void send_message(struct rdma_cm_id *id)
 {
-  struct conn_context *ctx = (struct conn_context *)id->context;
+	struct conn_context *ctx = (struct conn_context *)id->context;
 
-  struct ibv_send_wr wr, *bad_wr = NULL;
-  struct ibv_sge sge;
+	struct ibv_send_wr wr, *bad_wr = NULL;
+	struct ibv_sge sge;
 
-  memset(&wr, 0, sizeof(wr));
+	memset(&wr, 0, sizeof(wr));
 
-  wr.wr_id = (uintptr_t)id;
-  wr.opcode = IBV_WR_SEND;
-  wr.sg_list = &sge;
-  wr.num_sge = 1;
-  wr.send_flags = IBV_SEND_SIGNALED;
+	wr.wr_id = (uintptr_t)id;
+	wr.opcode = IBV_WR_SEND;
+	wr.sg_list = &sge;
+	wr.num_sge = 1;
+	wr.send_flags = IBV_SEND_SIGNALED;
 
-  sge.addr = (uintptr_t)ctx->msg;
-  sge.length = sizeof(*ctx->msg);
-  sge.lkey = ctx->msg_mr->lkey;
+	sge.addr = (uintptr_t)ctx->send_msg;
+	sge.length = sizeof(*ctx->send_msg);
+	sge.lkey = ctx->send_msg_mr->lkey;
 
-  TEST_NZ(ibv_post_send(id->qp, &wr, &bad_wr));
+	TEST_NZ(ibv_post_send(id->qp, &wr, &bad_wr));
 }
 
-static void post_receive(struct rdma_cm_id *id)
+static void post_msg_receive(struct rdma_cm_id *id)
 {
-  struct ibv_recv_wr wr, *bad_wr = NULL;
+	struct conn_context *ctx = (struct conn_context *)id->context;
 
-  memset(&wr, 0, sizeof(wr));
+	struct ibv_recv_wr wr, *bad_wr = NULL;
+	struct ibv_sge sge;
 
-  wr.wr_id = (uintptr_t)id;
-  wr.sg_list = NULL;
-  wr.num_sge = 0;
+	memset(&wr, 0, sizeof(wr));
 
-  TEST_NZ(ibv_post_recv(id->qp, &wr, &bad_wr));
+	wr.wr_id = (uintptr_t)id;
+	wr.sg_list = &sge;
+	wr.num_sge = 1;
+
+	sge.addr = (uintptr_t) ctx->recv_msg;
+	sge.length = sizeof(*ctx->recv_msg);
+	sge.lkey = ctx->recv_msg_mr->lkey;
+
+	TEST_NZ(ibv_post_recv(id->qp, &wr, &bad_wr));
+}
+
+static void post_rmem_receive(struct rdma_cm_id *id)
+{
+	struct ibv_recv_wr wr, *bad_wr = NULL;
+
+	memset(&wr, 0, sizeof(wr));
+
+	wr.wr_id = (uintptr_t) id;
+	wr.sg_list = NULL;
+	wr.num_sge = 0;
+
+	TEST_NZ(ibv_post_recv(id->qp, &wr, &bad_wr));
 }
 
 static void on_pre_conn(struct rdma_cm_id *id)
 {
-  struct conn_context *ctx = (struct conn_context *)malloc(sizeof(struct conn_context));
+	struct conn_context *ctx = (struct conn_context *)malloc(sizeof(struct conn_context));
 
-  id->context = ctx;
+	id->context = ctx;
 
-  ctx->file_name[0] = '\0'; // take this to mean we don't have the file name
+	TEST_Z(ctx->rmem_mr = ibv_reg_mr(
+			rc_get_pd(), rmem.mem, RMEM_SIZE,
+			IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_WRITE |
+			IBV_ACCESS_REMOTE_READ));
 
-  posix_memalign((void **)&ctx->buffer, sysconf(_SC_PAGESIZE), BUFFER_SIZE);
-  TEST_Z(ctx->buffer_mr = ibv_reg_mr(rc_get_pd(), ctx->buffer, BUFFER_SIZE, IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_WRITE));
+	posix_memalign((void **)&ctx->recv_msg, sysconf(_SC_PAGESIZE), sizeof(*ctx->recv_msg));
+	TEST_Z(ctx->recv_msg_mr = ibv_reg_mr(
+			rc_get_pd(), ctx->recv_msg, sizeof(*ctx->recv_msg),
+			IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_WRITE));
 
-  posix_memalign((void **)&ctx->msg, sysconf(_SC_PAGESIZE), sizeof(*ctx->msg));
-  TEST_Z(ctx->msg_mr = ibv_reg_mr(rc_get_pd(), ctx->msg, sizeof(*ctx->msg), IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_WRITE));
+	posix_memalign((void **)&ctx->send_msg, sysconf(_SC_PAGESIZE), sizeof(*ctx->send_msg));
+	TEST_Z(ctx->send_msg_mr = ibv_reg_mr(
+			rc_get_pd(), ctx->send_msg, sizeof(*ctx->send_msg),
+			IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_READ));
 
-  post_receive(id);
+	post_msg_receive(id);
 }
 
 static void on_connection(struct rdma_cm_id *id)
 {
-  struct conn_context *ctx = (struct conn_context *)id->context;
+	struct conn_context *ctx = (struct conn_context *)id->context;
 
-  ctx->msg->id = MSG_MR;
-  ctx->msg->data.mr.addr = (uintptr_t)ctx->buffer_mr->addr;
-  ctx->msg->data.mr.rkey = ctx->buffer_mr->rkey;
+	ctx->send_msg->id = MSG_MR;
+	ctx->send_msg->data.mr.addr = (uintptr_t)ctx->rmem_mr->addr;
+	ctx->send_msg->data.mr.rkey = ctx->rmem_mr->rkey;
 
-  send_message(id);
+	send_message(id);
+	post_rmem_receive(id);
 }
 
 static void on_completion(struct ibv_wc *wc)
 {
-  struct rdma_cm_id *id = (struct rdma_cm_id *)(uintptr_t)wc->wr_id;
-  struct conn_context *ctx = (struct conn_context *)id->context;
+	struct rdma_cm_id *id = (struct rdma_cm_id *)(uintptr_t)wc->wr_id;
+	struct conn_context *ctx = (struct conn_context *)id->context;
 
-  if (wc->opcode == IBV_WC_RECV_RDMA_WITH_IMM) {
-    uint32_t size = ntohl(wc->imm_data);
+	if (wc->opcode == IBV_WC_RECV_RDMA_WITH_IMM) {
+		post_rmem_receive(id);
+		return;
+	}
 
-    if (size == 0) {
-      ctx->msg->id = MSG_DONE;
-      send_message(id);
+	if (wc->opcode == IBV_WC_RECV) {
+		struct message *msg = ctx->recv_msg;
+		void *ptr;
+		uint64_t offset;
 
-      // don't need post_receive() since we're done with this connection
-
-    } else if (ctx->file_name[0]) {
-      ssize_t ret;
-
-      printf("received %i bytes.\n", size);
-
-      ret = write(ctx->fd, ctx->buffer, size);
-
-      if (ret != size)
-        rc_die("write() failed");
-
-      post_receive(id);
-
-      ctx->msg->id = MSG_READY;
-      send_message(id);
-
-    } else {
-      memcpy(ctx->file_name, ctx->buffer, (size > MAX_FILE_NAME) ? MAX_FILE_NAME : size);
-      ctx->file_name[size - 1] = '\0';
-
-      printf("opening file %s\n", ctx->file_name);
-
-      ctx->fd = open(ctx->file_name, O_WRONLY | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-
-      if (ctx->fd == -1)
-        rc_die("open() failed");
-
-      post_receive(id);
-
-      ctx->msg->id = MSG_READY;
-      send_message(id);
-    }
-  }
+		switch (msg->id) {
+		case MSG_ALLOC:
+			TEST_NZ(pthread_mutex_lock(&alloc_mutex));
+			ptr = rmem_alloc(&rmem, msg->data.alloc.size,
+					msg->data.alloc.tag);
+			TEST_NZ(pthread_mutex_unlock(&alloc_mutex));
+			offset = ptr - rmem.mem;
+			ctx->send_msg->id = MSG_MEMRESP;
+			ctx->send_msg->data.memresp.offset = offset;
+			ctx->send_msg->data.memresp.error = (ptr != NULL);
+			send_message(id);
+			post_msg_receive(id);
+			break;
+		case MSG_LOOKUP:
+			ptr = rmem_lookup(&rmem, msg->data.lookup.tag);
+			offset = ptr - rmem.mem;
+			ctx->send_msg->id = MSG_MEMRESP;
+			ctx->send_msg->data.memresp.offset = offset;
+			ctx->send_msg->data.memresp.error = (ptr != NULL);
+			send_message(id);
+			post_msg_receive(id);
+			break;
+		case MSG_FREE:
+			offset = msg->data.free.offset;
+			rmem_free(&rmem, rmem.mem + offset);
+			post_msg_receive(id);
+			break;
+		default:
+			fprintf(stderr, "Invalid message type %d\n", msg->id);
+			exit(EXIT_FAILURE);
+		}
+	}
 }
 
 static void on_disconnect(struct rdma_cm_id *id)
 {
-  struct conn_context *ctx = (struct conn_context *)id->context;
+	struct conn_context *ctx = (struct conn_context *)id->context;
 
-  close(ctx->fd);
+	ibv_dereg_mr(ctx->rmem_mr);
+	ibv_dereg_mr(ctx->recv_msg_mr);
+	ibv_dereg_mr(ctx->send_msg_mr);
 
-  ibv_dereg_mr(ctx->buffer_mr);
-  ibv_dereg_mr(ctx->msg_mr);
+	free(ctx->send_msg);
+	free(ctx->recv_msg);
 
-  free(ctx->buffer);
-  free(ctx->msg);
-
-  printf("finished transferring %s\n", ctx->file_name);
-
-  free(ctx);
+	free(ctx);
 }
 
 int main(int argc, char **argv)
 {
-  rc_init(
-    on_pre_conn,
-    on_connection,
-    on_completion,
-    on_disconnect);
+	init_rmem_table(&rmem);
+	pthread_mutex_init(&alloc_mutex, NULL);
 
-  printf("waiting for connections. interrupt (^C) to exit.\n");
+	rc_init(
+		on_pre_conn,
+		on_connection,
+		on_completion,
+		on_disconnect);
 
-  rc_server_loop(DEFAULT_PORT);
+	printf("waiting for connections. interrupt (^C) to exit.\n");
 
-  return 0;
+	rc_server_loop(DEFAULT_PORT);
+
+	free_rmem_table(&rmem);
+	pthread_mutex_destroy(&alloc_mutex);
+
+	return 0;
 }
