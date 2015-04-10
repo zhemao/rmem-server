0bbd9c9c51fae189f48d9aef75641fc4847f5a35
Howard Mao <zhehao.mao@gmail.com>
2015-04-09 17:50:05 -0700

    don't forget to add messages.h changes

--- messages.h
+++ messages.h
@@ -4,26 +4,39 @@
 const char *DEFAULT_PORT = "12345";
 const size_t BUFFER_SIZE = 10 * 1024 * 1024;
 
-enum message_id
-{
-  MSG_INVALID = 0,
-  MSG_MR,
-  MSG_READY,
-  MSG_DONE
+enum message_id {
+	MSG_INVALID = 0,
+	MSG_MR,
+	MSG_ALLOC,
+	MSG_LOOKUP,
+	MSG_FREE,
+	MSG_MEMRESP,
+	MSG_WACK
 };
 
-struct message
-{
-  int id;
+struct message {
+	enum message_id id;
 
-  union
-  {
-    struct
-    {
-      uint64_t addr;
-      uint32_t rkey;
-    } mr;
-  } data;
+	union {
+		struct {
+			uint64_t addr;
+			uint32_t rkey;
+		} mr;
+		struct {
+			uint64_t size;
+			uint32_t tag;
+		} alloc;
+		struct {
+			uint64_t offset;
+		} free;
+		struct {
+			uint32_t tag;
+		} lookup;
+		struct {
+			uint64_t offset;
+			int8_t error;
+		} memresp;
+	} data;
 };
 
 #endif
