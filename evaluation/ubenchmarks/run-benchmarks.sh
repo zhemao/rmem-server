9341cd4dfdc9060e906bf4bc088681c270a43bce
Howard Mao <zhehao.mao@gmail.com>
2015-05-06 12:12:03 -0700

    use multiple trials in run-benchmarks script

--- evaluation/ubenchmarks/run-benchmarks.sh
+++ evaluation/ubenchmarks/run-benchmarks.sh
@@ -5,10 +5,12 @@ CLIENT=f1
 SERVER=f2
 PORT=1234
 
-for i in {1..20}; do
+function start_server {
     ssh -f $SERVER "$RMEM_DIR/rmem-server $PORT &> /dev/null"
     sleep 1
-    ssh $CLIENT "$UBM_DIR/commit-bm $SERVER $PORT $i" | tail -n 1
+}
+
+function stop_server {
     ssh $SERVER 'kill `cat /tmp/rmem-server.pid`'
     # wait for server process to die
     while true; do
@@ -17,18 +19,26 @@ for i in {1..20}; do
             break
         fi
     done
+}
+
+for i in {1..20}; do
+    printf "%d" $i
+    for trial in {1..3}; do
+        start_server
+        result=$(ssh $CLIENT "$UBM_DIR/commit-bm $SERVER $PORT $i" | tail -n 1)
+        printf ",%f" $result
+        stop_server
+    done
+    printf "\n"
 done > commit-results.csv
 
 for i in {1..20}; do
-    ssh -f $SERVER "$RMEM_DIR/rmem-server $PORT &> /dev/null"
-    sleep 1
-    ssh $CLIENT "$UBM_DIR/recovery-bm $SERVER $PORT $i" | tail -n 1
-    ssh $SERVER 'kill `cat /tmp/rmem-server.pid`'
-    # wait for server process to die
-    while true; do
-        SERVER_PID=$(lsof -t -i TCP@$SERVER:22)
-        if [ -n $SERVER_PID ]; then
-            break
-        fi
+    printf "%d" $i
+    for trial in {1..3}; do
+        start_server
+        result=$(ssh $CLIENT "$UBM_DIR/recovery-bm $SERVER $PORT $i" | tail -n 1)
+        printf ",%f" $result
+        stop_server
     done
+    printf "\n"
 done > recovery-results.csv
