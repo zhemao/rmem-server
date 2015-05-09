4ae79020b2a95a42255fe28221591eb9e649ee5d
Howard Mao <zhehao.mao@gmail.com>
2015-05-08 19:25:59 -0700

    run_to_success should take a setup command and a recovery command

--- evaluation/dgemv/run_to_success.py
+++ evaluation/dgemv/run_to_success.py
@@ -1,18 +1,24 @@
 import sys
 import subprocess
 import time
+import shlex
+import os
 
-def run_to_completion(cmd_args):
-    ret = -1
-    totaltime = 0.0
+def run_to_completion(setup_cmd, run_cmd):
+    devnull = open(os.devnull, "w")
+    start = time.time()
+    ret = subprocess.call(shlex.split(setup_cmd), stdout=devnull)
+    end = time.time()
+    totaltime = (end - start)
 
     while ret != 0:
         start = time.time()
-        ret = subprocess.call(cmd_args)
+        ret = subprocess.call(shlex.split(run_cmd), stdout=devnull)
         end = time.time()
         totaltime += (end - start)
 
     print(totaltime)
+    devnull.close()
 
 if __name__ == "__main__":
-    run_to_completion(sys.argv[1:])
+    run_to_completion(*sys.argv[1:])
