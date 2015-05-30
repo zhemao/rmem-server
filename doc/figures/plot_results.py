070d16c131ae2a3a85e7e12f415b4981d0c0de2e
Howard Mao <zhehao.mao@gmail.com>
2015-05-30 16:14:43 -0700

    combine micro-benchmark plots

--- doc/figures/plot_results.py
+++ doc/figures/plot_results.py
@@ -4,23 +4,35 @@ import matplotlib.pyplot as plt
 import numpy as np
 from os.path import splitext
 
-def main():
-    if len(sys.argv) < 2:
-        return "Usage: plot_results.py results.csv"
-
-    infname = sys.argv[1]
-    df = pandas.read_csv(infname)
+def plot_file(fname, label):
+    df = pandas.read_csv(fname)
 
     x = df.iloc[:,0].values
     y = np.mean(df.iloc[:,1:].values, 1)
 
-    outname, _ = splitext(infname)
-    outname += ".pdf"
+    plt.plot(x, y, label=label)
+
+def main():
+    if len(sys.argv) < 5:
+        return "Usage: plot_results.py output.pdf rm.csv rc.csv blcr.csv"
+
+    outfname = sys.argv[1]
+    infnames = sys.argv[2:]
+
+    labels = ["Infiniband", "RAMCloud", "BLCR"]
+
+    for fname, label in zip(infnames, labels):
+        plot_file(fname, label)
+
+    if "commit" in outfname:
+        plt.title("Commit Micro-benchmark")
+    else:
+        plt.title("Recovery Micro-benchmark")
 
     plt.xlabel("Num. Pages")
     plt.ylabel("Time (s)")
-    plt.plot(x, y)
-    plt.savefig(outname)
+    plt.legend()
+    plt.savefig(outfname)
 
 if __name__ == "__main__":
     sys.exit(main())
